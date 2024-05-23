476a5e9c7b06b878d0ba3550c4a30a5949e20be6
lucau <lu.cauner@gmail.com>
2024-05-23 20:24:01 +0200

    vite config updated to docker

--- LiveTrackerWebApp/vite.config.ts
+++ LiveTrackerWebApp/vite.config.ts
@@ -4,15 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 
 //docker
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     host: true,
-//     port: 8070
-//   }
-// })
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 8070
+  }
+})
 
 //local
-export default defineConfig({
-  plugins: [react()]
-})
+// export default defineConfig({
+//   plugins: [react()]
+// })
